d0c48723bcfe29cd81fee988ff68e4c3746c3328
VarunDev-ai <varunsahi7@gmail.com>
2025-12-11 16:40:14 +0530

    created header: navitems and userdropdown menu

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+export const IMAGE = {
+  logo: "/assets/icons/logo.svg",
+  star: "/assets/icons/star.svg",
+};
+
+export const NAV_ITEMS: { href: string; title: string }[] = [
+  { href: "/", title: "Dashboard" },
+  { href: "/search", title: "Search" },
+  { href: "/watchlist", title: "Watchlist" },
+];
+
+export const USER_IMAGE_URL =
+  "https://media.licdn.com/dms/image/v2/D5603AQHSS4XgZBrNcw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726741725535?e=1767225600&v=beta&t=k8RbiNrtjTfRRxhhHixuoUPS-Dk6yUo0KVGvPU2paI8";
